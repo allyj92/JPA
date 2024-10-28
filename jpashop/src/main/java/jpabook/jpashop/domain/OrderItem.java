3a8aa87eb2fde0fc90c9ca3ef603497d9f7e8bf9
eunjaelim <ieunjae361@gmail.com>
2024-10-29 01:47:19 +0900

    연관관계 맵핑

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item; // 주문 가격
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order; // 주문 수량
+
+    private int orderPrice; // 주문 가격
+    private int count; // 주문 수량
+
+
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    // 카테고리
+
+    @OneToMany
+    List<OrderItem> orderItem = new ArrayList<>();
+}
