17bc6bc5ba51e8ca98c1155b2390831060bcbef4
eunjaelim <ieunjae361@gmail.com>
2024-11-27 22:50:55 +0900

    타임리프

--- jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -4,13 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello(Model model){
-        model.addAttribute("data","hello!!!");
+        model.addAttribute("name","타임리프 연습");
         return "hello";
     }
+
+    @GetMapping("/users")
+    public String users(Model model){
+        List<String> userList = Arrays.asList("홍길동","김영희","이철수");
+        model.addAttribute("users",userList);
+        return "users";
+    }
+
+
 
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/controller/FormControlle.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.controller;
+
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormControlle {
+    @GetMapping("/form")
+    public String formPage(Model model){
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String submitForm(@ModelAttribute User user, Model model){
+        model.addAttribute("submittedUser",user);
+        return "form-result";
+    }
+
+    @Getter @Setter
+    class User{
+        private String name;
+        private String email;
+    }
+}
